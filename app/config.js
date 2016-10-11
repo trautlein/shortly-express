dfa7195c5079f43ef2df13f4d69fc1dd0fcedc68
trautlein <hans@trautlein.com>
2016-10-10 17:26:20 -0700

    Add hashed password persistence to user

--- app/config.js
+++ app/config.js
@@ -40,5 +40,17 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.hasTable('users'). then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255).unique();
+      user.string('password', 255);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,36 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  verifyPassword: function(attemptedPassword) {
+    return new Promise ( function (resolve, reject) {
+      bcrypt.compare(this.get('password'), attemptedPassword, function(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+
+  hashPassword: function(model, attrs, options) {
+    return new Promise( function (resolve, reject) {
+      bcrypt.hash(model.attributes.password, null, console.log.bind(this), function (err, hash) {
+        if (err) { 
+          reject(err); 
+        } else {
+          model.set('password', hash);
+          resolve(hash);
+        }
+      });
+    });
+  },
+
+  initialize: function() {
+    this.on('creating', this.hashPassword, this);
+  }
 });
 
 module.exports = User;
